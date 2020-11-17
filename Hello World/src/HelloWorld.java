bc37982eb62929eb42fecea85bc246b9cec408bf
82107 <82107@DESKTOP-160INVR>
2020-11-17 20:30:08 +0900

    added master print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Jin Young Seo");
+		System.out.println("20192825");
 	}
 
 }
