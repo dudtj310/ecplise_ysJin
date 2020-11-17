d0ce7a1703f61a5651e1ff580becd8e7b981af3e
82107 <82107@DESKTOP-160INVR>
2020-11-17 21:34:34 +0900

    create print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Jin Young Seo");
 		System.out.println("20192825");
 		System.out.println("컴퓨터소프트웨어공학과");
-		System.out.println("sophomore");
+		
 	}
 
 }
