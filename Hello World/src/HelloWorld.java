3e10a6016cfc6ba5ffa8d1773b43b32d71586214
82107 <82107@DESKTOP-160INVR>
2020-11-17 20:31:45 +0900

    added ysJin print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Jin Young Seo");
+		System.out.println("컴퓨터소프트웨어공학과");
 	}
 
 }
